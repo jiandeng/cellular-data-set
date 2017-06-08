18fb5676af308b9459a0fc143e4e4efa27f79888
jiandeng <jiandeng.develop@gmail.com>
2017-06-08 09:16:06 +0800

    Bugfix for column name

--- cell_util.py
+++ cell_util.py
@@ -13,9 +13,15 @@ def cell_to_dataframe(file):
     content = map(lambda x: x if '-->>' not in x else x[25:], content)
     records = [ unpack('i'*33, de(line[:-1], '-/'))[1:-3] for line in content ]
     cn = ['sn', 'start_time', 'hwt_time', 'csq_time', 'creg_time', 'csq', 'pdp_time', 'connect_time']
-    cn.extend(reduce(operator.concat, [['upload_time' + str(i), 'download_time' + str(i)] for i in range(10)]))
+    cn.extend(['upload_time' + str(i) for i in range(10)])
+    cn.extend(['download_time' + str(i) for i in range(10)])
     cn.append('voltage')
     df = pd.DataFrame(records, columns=cn)
+
+    cn = ['sn', 'start_time', 'hwt_time', 'csq_time', 'creg_time', 'csq', 'pdp_time', 'connect_time']
+    cn.extend(reduce(operator.concat, [['upload_time' + str(i), 'download_time' + str(i)] for i in range(10)]))
+    cn.append('voltage')
+    df = df[cn]
 
     df.where(df != -1, np.nan, inplace=True)
     for col in df:
