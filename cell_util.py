51b9503682feff8686bcd6c2a274ac79bdf61f0e
jiandeng <jiandeng.develop@gmail.com>
2017-06-09 10:29:25 +0800

    Update column order

--- cell_util.py
+++ cell_util.py
@@ -12,27 +12,29 @@ def cell_to_dataframe(file):
 
     content = map(lambda x: x if '-->>' not in x else x[25:], content)
     records = [ unpack('i'*33, de(line[:-1], '-/'))[1:-3] for line in content ]
-    cn = ['sn', 'start_time', 'hwt_time', 'csq_time', 'creg_time', 'csq', 'pdp_time', 'connect_time']
+    cn = ['sn', 'time', 'hwt_time', 'csq_time', 'creg_time', 'csq', 'pdp_time', 'connect_time']
     cn.extend(['upload_time' + str(i) for i in range(10)])
     cn.extend(['download_time' + str(i) for i in range(10)])
     cn.append('voltage')
     df = pd.DataFrame(records, columns=cn)
 
-    cn = ['sn', 'start_time', 'hwt_time', 'csq_time', 'creg_time', 'csq', 'pdp_time', 'connect_time']
+    cn = ['sn', 'time', 'voltage', 'csq', 'hwt_time', 'csq_time', 'creg_time', 'pdp_time', 'connect_time']
     cn.extend(reduce(operator.concat, [['upload_time' + str(i), 'download_time' + str(i)] for i in range(10)]))
-    cn.append('voltage')
     df = df[cn]
 
     df.where(df != -1, np.nan, inplace=True)
     for col in df:
-        if col != 'start_time' and 'time' in col:
-            df[col] -= df['start_time']
+        if '_time' in col:
+            df[col] -= df['time']
+
+    df['time'] = pd.to_datetime(df.time, unit = 's')
+    df.sort_values(['sn', 'time'], inplace=True)
 
     return df
 
 
 if __name__ == '__main__':
     df = cell_to_dataframe(sys.argv[1])
-    print df.tail()
+    df.to_csv(sys.argv[1].replace('txt', 'csv'))
 
 
